bb3597f07783a0405445e8a6ab0df83ee844d424
renmurakami <autoeyy@gmail.com>
2022-06-11 09:10:26 +0900

    nav

---
+++ lib/components/navText.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class NavText extends StatelessWidget {
+  const NavText({Key? key, required this.navInnerText, required this.isSelect})
+      : super(key: key);
+
+  final String navInnerText;
+  final bool isSelect;
+
+  @override
+  Widget build(BuildContext context) {
+    return Opacity(
+      opacity: isSelect ? 1 : 0.3,
+      child: Column(
+        children: [
+          Text(
+            navInnerText,
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Visibility(
+            visible: isSelect,
+            child: Container(
+              width: 8,
+              height: 8,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.black87,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/views/discover.dart
+++ lib/views/discover.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_style/components/navText.dart';
 import 'package:flutter_style/components/userBox.dart';
 
 class DiscoverView extends StatelessWidget {
@@ -9,6 +10,7 @@ class DiscoverView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appBarHeight = AppBar().preferredSize.height;
+    var _screenSize = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         leading: Icon(Icons.list),
@@ -20,6 +22,51 @@ class DiscoverView extends StatelessWidget {
             padding: EdgeInsets.symmetric(vertical: appBarHeight * 0.1),
             child: UserBox(appBarHeight: appBarHeight),
           ),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: EdgeInsets.only(
+              top: appBarHeight * 0.3,
+              left: _screenSize.width * 0.05,
+            ),
+            child: const Text(
+              "Discover",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 32,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(
+                left: _screenSize.width * 0.05, top: appBarHeight * 0.4),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                NavText(
+                  navInnerText: "Place",
+                  isSelect: true,
+                ),
+                SizedBox(
+                  width: _screenSize.width * 0.05,
+                ),
+                NavText(
+                  navInnerText: "Inspiration",
+                  isSelect: false,
+                ),
+                SizedBox(
+                  width: _screenSize.width * 0.05,
+                ),
+                NavText(
+                  navInnerText: "Emotions",
+                  isSelect: false,
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
